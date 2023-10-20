a75b612ba4749757b52807d715b0d5085497147a
Timothy Hoyt <timothyhoytbsme@gmail.com>
2023-10-20 13:56:16 -0500

    Form setup

--- js/index.js
+++ js/index.js
@@ -11,7 +11,9 @@ window.onscroll = function () {
   
 function requestMethodChanged(){
     const phone = document.getElementById('request-number');
+    const phoneLabel = document.getElementById('label-for-request-number');
     const email = document.getElementById('request-email');
+    const emailLabel = document.getElementById('label-for-request-email');
 
     const options = document.getElementsByName('method-choice');
     let checked = undefined;
@@ -23,12 +25,57 @@ function requestMethodChanged(){
     switch(checked.id){
         case 'by-email':
             phone.style.display = "none";
+            phoneLabel.style.display = "none";
+
             email.style.display = "block";
+            emailLabel.style.display = "block";
+            
             break;
         default:
-            email.style.display = "none";
             phone.style.display = "block";
+            phoneLabel.style.display = "block";
+            email.style.display = "none";
+            emailLabel.style.display = "none";
+
             break;
 
     }
+}
+
+
+var form = document.getElementById('request-form');
+form.onsubmit = function(event){
+    event.preventDefault();
+    var xhr = new XMLHttpRequest();
+    var fd = new FormData(form);
+    let fdjson = JSON.stringify(Object.fromEntries(fd))
+    console.log("atttempting to send",fdjson)
+
+    //todo use actual api link
+    xhr.open('POST','https://jsonplaceholder.typicode.com/posts')
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onerror = function () {
+        alert('An unknown error occurred when attempting to send your request. We apologize for the inconvenience.')
+    };
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log("response status", xhr.status)
+
+            if(xhr.status == 201 || xhr.status == 200 ){
+                console.log("SUCCESS")
+            }else{
+                console.log("FAIL")
+            }
+            console.log(xhr.response)
+
+            form.reset();
+
+        }
+    }
+
+    xhr.send(fdjson);
+
+    return false; 
 }
